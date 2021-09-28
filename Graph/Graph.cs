690956bb614d04bcde9a801ad845a4138dd785af
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-09-28 21:56:30 +0300

    update graph, add isConnected and getDistance

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -6,27 +6,96 @@ using System.Threading.Tasks;
 
 namespace Lab1.Graph
 {
-    class Graph<TVertexData, TEdges, TEdgeData> where TEdges : IEdges<TEdgeData>, new()
+    internal class Graph<TEdges, TVertexData, TEdgeData> where TEdges : Edges.IEdges<TEdgeData>, new()
     {
-        private readonly List<Vertex<TVertexData>> vertices;
-        private readonly IEdges<TEdgeData> connections;
+        public readonly Vertices<TVertexData> vertices;
+        public readonly Edges.IEdges<TEdgeData> edges;
 
         public Graph()
         {
-            this.vertices = new List<Vertex<TVertexData>>();
-            this.connections = new TEdges();
+            vertices = new Vertices<TVertexData>();
+            edges = new TEdges();
         }
 
         public int AddVertex(TVertexData data)
         {
-            vertices.Add(new Vertex<TVertexData>(data));
-            return connections.AddVertex();
+            vertices.Add(data);
+            return edges.AddVertex();
         }
 
         public void RemoveVertex(int index)
         {
-            vertices.RemoveAt(index);
-            connections.RemoveVertex(index);
+            vertices.Remove(index);
+            edges.RemoveVertex(index);
+        }
+
+        public bool IsConnected()
+        {
+            if (vertices.Count() == 0)
+            {
+                return true;
+            }
+
+            List<bool> used = Enumerable.Repeat(false, vertices.Count()).ToList();
+            Queue<int> queue = new();
+            int counter = 1;
+            used[0] = true;
+            queue.Enqueue(0);
+
+            while (queue.Count != 0)
+            {
+                int vertexOne = queue.Dequeue();
+                foreach ((int, TEdgeData) i in edges.GetVertexEdgesEnumerator(vertexOne))
+                {
+                    if (!used[i.Item1])
+                    {
+                        counter += 1;
+                        if (counter == vertices.Count())
+                        {
+                            return true;
+                        }
+                        used[i.Item1] = true;
+                        queue.Enqueue(i.Item1);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public (bool, int) GetDistance(int vertexOne, int vertexTwo)
+        {
+            List<bool> used = Enumerable.Repeat(false, vertices.Count()).ToList();
+            Queue<int> queue = new();
+            used[vertexOne] = true;
+            queue.Enqueue(vertexOne);
+            int distance = 0;
+            int lastVertex = vertexOne; // lastVertex with such distance
+
+            while (queue.Count != 0)
+            {
+                int next = queue.Dequeue();
+                if (next == vertexTwo)
+                {
+                    return (true, distance);
+                }
+
+                foreach ((int, TEdgeData) i in edges.GetVertexEdgesEnumerator(next))
+                {
+                    if (!used[i.Item1])
+                    {
+                        used[i.Item1] = true;
+                        queue.Enqueue(i.Item1);
+                    }
+                }
+
+                if (next == lastVertex)
+                {
+                    distance += 1;
+                    lastVertex = queue.LastOrDefault();
+                }
+            }
+            return (false, default);
         }
     }
 }
