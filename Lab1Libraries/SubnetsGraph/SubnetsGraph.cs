52b126704be0cd0b67c6f2c92cd0690da3828a88
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-10-06 09:42:37 +0300

    subnet graph

---
+++ Lab1Libraries/SubnetsGraph/SubnetsGraph.cs
@@ -0,0 +1,18 @@
+﻿using Lab1Libraries.Graph;
+using Lab1Libraries.IP;
+
+namespace Lab1Libraries.SubnetsGraph
+{
+    public class SubnetsGraph : Graph<Graph.Edges.AdjacencyList<object>, IPv6, object>
+    {
+        public void AddSafeEdge(int vertexOne, int vertexTwo)
+        {
+            IPv6 a = vertices[vertexOne];
+            IPv6 b = vertices[vertexTwo];
+            if (a.HasSubnet(b) || b.HasSubnet(a))
+            {
+                edges.Add(vertexOne, vertexTwo, default);
+            }
+        }
+    }
+}
